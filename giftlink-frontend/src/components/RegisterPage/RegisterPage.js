29801ac1c26dc576c16ea048269f58996a6ea71d
Lisa Brown <40493724+librowndev@users.noreply.github.com>
2026-04-25 15:54:35 -0400

    Implement the frontend and backend for login and user profiles. Frontend and backend work locally.

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-// Step 1 - Task 1
-import { urlConfig } from '../../config';
-// Step 1 - Task 2
+//Step 1 - Task 1
+import {urlConfig} from '../../config';
+
+//Step 1 - Task 2
 import { useAppContext } from '../../context/AuthContext';
-// Step 1 - Task 3
+
+//Step 1 - Task 3
 import { useNavigate } from 'react-router-dom';
+
 import './RegisterPage.css';
 
 function RegisterPage() {
@@ -13,42 +16,50 @@ function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    //Step 1 - Task 4
+    const [showerr, setShowerr] = useState('');
+
+    //Step 1 - Task 5
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
+
     const handleRegister = async () => {
         const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
-            // Step 1 - Task 6
+            //Step 1 - Task 6
             method: 'POST',
-            // Step 1 - Task 7
+            //Step 1 - Task 7
             headers: {
                 'content-type': 'application/json',
             },
-            // Step 1 - Task 8
+            //Step 1 - Task 8
             body: JSON.stringify({
                 firstName: firstName,
                 lastName: lastName,
                 email: email,
-                password: password,
-            }),
+                password: password
+            })
         });
 
-        // Step 2 - Task 1
+        //Step 2 - Task 1
         const json = await response.json();
+        console.log('json data', json);
+        console.log('er', json.error);
 
-        // Step 2 - Task 2
+        //Step 2 - Task 2
         if (json.authtoken) {
             sessionStorage.setItem('auth-token', json.authtoken);
             sessionStorage.setItem('name', firstName);
             sessionStorage.setItem('email', json.email);
-            // Step 2 - Task 3
+            //Step 2 - Task 3
             setIsLoggedIn(true);
-            // Step 2 - Task 4
+            //Step 2 - Task 4
             navigate('/app');
         }
-
-        // Step 2 - Task 5
         if (json.error) {
+            //Step 2 - Task 5
             setShowerr(json.error);
         }
-    };
+    }
 
     return (
         <div className="container mt-5">
@@ -93,6 +104,9 @@ function RegisterPage() {
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
+                            {/* Step 2 - Task 6*/}
+
+                            <div className="text-danger">{showerr}</div>
                         </div>
 
                         <div className="mb-4">
